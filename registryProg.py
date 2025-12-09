6d997e561857551ed56a3fe9335cd87036453f43
tephL <rhezstephen06@gmail.com>
2025-12-09 10:12:08 +0800

    Added: Invalid input handling through while-loop and if-else; Name input; Format specifier for output.

--- registryProg.py
+++ registryProg.py
@@ -1,20 +1,38 @@
 #User Registry
+print("Hello, Register now!")
+name = input("\nName: ")
 
-phnum = input("Phone Num: ")
+# Num anonymizer
+numStatus = False
 
-if len(phnum) == 11:
-    first4 = phnum[0:4]
-    last2 = phnum[-2:]
-    print(f"Anon num: {first4}-XXX-XX{last2}")
-else:
-    print("invalid")
+while not numStatus:
+    phnum = input("\nPhone Num: ")
     
+    if len(phnum) == 11:
+        first4 = phnum[0:4]
+        last2 = phnum[-2:]
+        anonNum = f"{first4}-XXX-XX{last2}"
+        numStatus = True
+    else:
+        print("Try again.\n")
+        
 # email slicer
-
-email = (input("Email: "))
-
-locAt = email.find("@")
-beforeAt = email[0:locAt]
-site = email[locAt:]
-
-print(f"{beforeAt} | ix@: {locAt} | {site}")
+mailStatus = False
+while not mailStatus:
+    email = (input("\nEmail: "))
+         
+    locAt = email.find("@")
+    beforeAt = email[0:locAt]
+    site = email[locAt+1:]
+    com = site[-4:]
+    
+    if email.find("@") and com==".com":
+        choppedMail = f"{beforeAt} | ix@: {locAt} | {site}"
+        mailStatus = True
+    else:
+        print("Invalid email.")
+        
+print("\nSuccessfully Registered!")
+print(f"\nHello {name}!")
+print(f"Phone Num: {anonNum:>10}")
+print(f"Email: {choppedMail:>10}")
