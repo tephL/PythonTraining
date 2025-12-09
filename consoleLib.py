63d9abfbc23f9dca36b2e5208c2bd716cb348c7d
tephL <rhezstephen06@gmail.com>
2025-12-09 15:01:13 +0800

    Improved console interface and book handling

--- consoleLib.py
+++ consoleLib.py
@@ -1,29 +1,38 @@
-books = []
-author = []
+books = ["Atomic Habits", "The Catcher in the Rye", "Bible", "Niggas vs Jews"]
+author = ["James Clear", "J.D. Sallinger", "Moses, etc.", "White W."]
 lists = [books, author]
 chc = True
 div = "====================="
 
 while chc:
-    chc = input(f"Library Menu\n{div}\n1. view books\n2. add books\n3. delete book\n{div}\n")
+    chc = input(f"{div}\nLibrary Menu\n{div}\n1. View books\n2. Add books\n3. Delete book\n0. Exit\n{div}\nChoice: ")
     print()
     
     if chc=="1":
-        print(f"Books\n{div}")
-        for x in range(0, len(books)):
-            print(f"{x+1}. {books[x]} by {author[x]}")
-        print(div)
+        if len(books) == 0:
+            print("Library is empty")
+        else:
+            print("----------------------------------------------------------")
+            print(f"{'Name':<33} | {'Author':<20} |")
+            print("----------------------------------------------------------")
+            for x in range(0, len(books)):
+                print(f"{x+1}. {books[x]:<30} | {author[x]:<20} |")
+            print("----------------------------------------------------------")
     elif chc=="2":
-        add = input("add: ")
+        add = input("Book: ")
         books.append(add)
-        auth = input("author: ")
+        auth = input("Author: ")
         author.append(auth)
     elif chc=="3":
         delete = input("name: ")
-        books.remove(delete)
+        if delete in books:
+            books.remove(delete)
+            print(f"Successfuly Deleted {delete}")
+        else:
+            print("Does not exist")
     elif chc=="0":
         chc = False
     else:
-        print("invalid")
+        print("Invalid")-0
         
     print()
